b3bd7701dc1a29b1c08f8fa4d53100b17bf4e233
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2025-11-13 10:52:43 +0100

    Correction

--- Langage/ex-jeu-object.js
+++ Langage/ex-jeu-object.js
@@ -4,74 +4,87 @@
 // vues précédemment (getRandom, getRandomArbitrary, getRandomInt, getRandomIntInclusive)
 // Remplacer la création de entierAlea par un appel à Random.getRandomIntInclusive(0, 100)
 
-function getRandom() {
-  return Math.random();
-}
-
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function getRandomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-}
-
-function getRandomIntInclusive(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-}
+// A EVITER, privilégier 4 fonctions exportées depuis un module
+const Random = {
+  getRandom() {
+    return Math.random();
+  },
+  getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  },
+  getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+  },
+};
 
 const readline = require('readline');
-
-function jouer() {
-  if (essais.length) {
-    console.log(`Vos essais précédents: ${essais.join(', ')}`);
-  }
-
-  rl.question('Entrez un nombre entre 0 et 100: ', (answer) => {
-    const userNumber = Number(answer);
-    console.log(`Vous avez entré: ${userNumber}`);
-
-    if (Number.isNaN(userNumber)) {
-      console.log('Erreur: Veuillez entrer un nombre valide.');
-      return jouer();
-    }
-
-    essais.push(userNumber);
-
-    if (entierAlea < userNumber) {
-      console.log("C'est plus petit !");
-      jouer();
-    } else if (entierAlea > userNumber) {
-      console.log("C'est plus grand !");
-      jouer();
-    } else {
-      console.log('Félicitations, vous avez trouvé le nombre !');
-      rl.close();
-    }
-  });
-}
 
 // Exercice 2
 // Créer une classe Jeu
 // dont le constructeur initialise rl, entierAlea et essais sous forme de propriétés de l'objet
 // et qui possède une méthode jouer() qui reprendra le code de la fonction jouer actuelle
 
-// On doit pouvoir créer la partie en faisant : 
+// On doit pouvoir créer la partie en faisant :
 // const jeu = new Jeu();
-// et lancer la partie en faisant : 
+// et lancer la partie en faisant :
 // jeu.jouer();
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+class Jeu {
+  constructor(options = {}) {
+    // const min = options && typeof options.min === 'number' ? options.min : 0;
+    // const max = options && typeof options.max === 'number' ? options.max : 100;
+    const min = options.min ?? 0;
+    const max = options.max ?? 100;
+
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    this.entierAlea = Random.getRandomIntInclusive(min, max);
+    this.essais = [];
+  }
+  jouer() {
+    if (this.essais.length) {
+      console.log(`Vos essais précédents: ${this.essais.join(', ')}`);
+    }
+
+    this.rl.question('Entrez un nombre entre 0 et 100: ', (answer) => {
+      const userNumber = Number(answer);
+      console.log(`Vous avez entré: ${userNumber}`);
+
+      if (Number.isNaN(userNumber)) {
+        console.log('Erreur: Veuillez entrer un nombre valide.');
+        return this.jouer();
+      }
+
+      this.essais.push(userNumber);
+
+      if (this.entierAlea < userNumber) {
+        console.log("C'est plus petit !");
+        this.jouer();
+      } else if (this.entierAlea > userNumber) {
+        console.log("C'est plus grand !");
+        this.jouer();
+      } else {
+        console.log('Félicitations, vous avez trouvé le nombre !');
+        this.rl.close();
+      }
+    });
+  }
+}
+
+const jeu = new Jeu({
+  min: 10,
+  max: 20,
 });
-const entierAlea = getRandomIntInclusive(0, 100);
-const essais = [];
-jouer();
+jeu.jouer();
 
 // Exercice 3
 // Modifier le constructeur de la classe Jeu
